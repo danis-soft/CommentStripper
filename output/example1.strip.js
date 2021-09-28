a382d2797873f6b1e8d77ab0a7cb750382d4e38d
danis-soft <dan_b18@yahoo.com>
2021-09-27 18:11:27 -0700

    Example and npm dependencies

---
+++ output/example1.strip.js
@@ -0,0 +1,14 @@
+/*@license
+	this is a test file for testing the comments removal of Comments remover
+	- 	This file is part of comment stripper program
+- 	that allows to strip comments C style (single and block) 
+- 	and whitespaces
+- Copyright (C) 2021 DaniS Software
+*/
+var abc = {};
+abc.push("krs");
+abc.push("adasdf"); //@license this a single line comment test that should not be removed
+abc.forEach((el)=>
+{
+	console.log(el);
+});
